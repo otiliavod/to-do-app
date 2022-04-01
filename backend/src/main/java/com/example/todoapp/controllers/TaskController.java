2a8ce0f8a2aebc0acd4b3d0eb23cf80ab3551182
Otilia Vodnitchi <otilia.vod@gmail.com>
2022-04-01 10:11:26 +0300

    added view tasks controller

--- backend/src/main/java/com/example/todoapp/controllers/TaskController.java
+++ backend/src/main/java/com/example/todoapp/controllers/TaskController.java
@@ -1,4 +1,26 @@
 package com.example.todoapp.controllers;
 
+import com.example.todoapp.domain.vo.Task;
+import com.example.todoapp.services.TaskService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/tasks")
+@RestController
 public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping(path="/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<Task> getUserTasks(@PathVariable Long userId) {
+      return this.taskService.getUserTasks(userId);
+    }
 }
--- backend/src/main/java/com/example/todoapp/dao/mybatis/task/TaskDAO.java
+++ backend/src/main/java/com/example/todoapp/dao/mybatis/task/TaskDAO.java
@@ -1,9 +1,11 @@
 package com.example.todoapp.dao.mybatis.task;
 
 import com.example.todoapp.domain.vo.Task;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskDAO {
      List<Task> getUserTasks(Long userId);
 }
