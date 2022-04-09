efb299c93a35bd0c5f8ff9b5a3e18ace72bc9a34
Otilia Vodnitchi <otilia.vod@gmail.com>
2022-04-09 18:14:08 +0300

    changed update profile service

--- backend/src/main/java/com/example/todoapp/services/ProfileService.java
+++ backend/src/main/java/com/example/todoapp/services/ProfileService.java
@@ -1,19 +1,21 @@
 package com.example.todoapp.services;
 
 import com.example.todoapp.dao.mybatis.user.UserDAO;
-import com.example.todoapp.domain.logic.UserChecksResponse;
 import com.example.todoapp.domain.vo.User;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileService {
     private final UserDAO userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public ProfileService(UserDAO userDao) {
+    public ProfileService(UserDAO userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
     public void updateProfile(User user) {
+        user.setPassword(this.passwordEncoder.encode(user.getPassword()));
         this.userDao.updateProfile(user);
     }
     public void deleteProfile(Long id) {
