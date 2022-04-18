42e601b276d6c0d1a211255e24a6a2004ca7333b
Alin Ivanoiu <alin.ivanoiu@alten.com>
2022-04-18 23:00:32 +0300

    updated pom.xml to build frontend with base-url

--- backend/src/main/java/com/example/todoapp/ToDoAppApplication.java
+++ backend/src/main/java/com/example/todoapp/ToDoAppApplication.java
@@ -2,9 +2,10 @@ package com.example.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class ToDoAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ToDoAppApplication.class, args);
